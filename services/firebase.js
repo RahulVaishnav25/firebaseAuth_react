bcbca41ade8632dae38adfe6f3c92acd7ee4f69c
Rahul Vaishnav <rahulvaishnav166@gmail.com>
2022-02-11 01:28:29 +0530

    sign in page add and firebase auth setup

---
+++ services/firebase.js
@@ -0,0 +1,15 @@
+// database/firebaseDb.js
+
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDZle_ApoW20BZUBlWVWG0WHNvSxmg-hPg",
+  authDomain: "fir-ecca9.firebaseapp.com",
+  projectId: "fir-ecca9",
+  storageBucket: "fir-ecca9.appspot.com",
+  messagingSenderId: "1068205041473",
+  appId: "1:1068205041473:web:96708bfa06306ba35ea3c0",
+  measurementId: "G-VKQT539YDF",
+});
+
+export default firebase;
